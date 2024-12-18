517c4af19159fdbdb7f4d60f6078ef92da3dff08
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-18 10:25:46 +0700

    Menambahkan Create dan Read pada Dosendao

---
+++ app/src/main/java/com/example/pertemuan12/dao/Dosendao.kt
@@ -0,0 +1,25 @@
+package com.example.pertemuan12.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.pertemuan12.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface Dosendao {
+    @Insert
+    suspend fun  insertDosen(dosen: Dosen)
+
+    //getAllMahasiswa
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    //getMahasiswa
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getDosen (nidn: String) : Flow<Dosen>
+
+
+}
