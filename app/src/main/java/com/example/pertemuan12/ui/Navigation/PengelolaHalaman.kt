46f4756f016e5f2068c614ed7165284bbc7c2dd1
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-18 18:54:02 +0700

    membuat fungsi PengelolaHalaman

---
+++ app/src/main/java/com/example/pertemuan12/ui/Navigation/PengelolaHalaman.kt
@@ -0,0 +1,31 @@
+package com.example.pertemuan12.ui.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan12.ui.view.MataKuliah.InsertMataKuliahView
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route) {
+        composable(
+            route = DestinasiHome.route
+        ){
+            InsertMataKuliahView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
