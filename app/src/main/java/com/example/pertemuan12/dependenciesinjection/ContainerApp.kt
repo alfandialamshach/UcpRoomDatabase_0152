89e574ea0c81e0647c42d7d354c4bbd55a7869f4
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-18 11:22:28 +0700

    menambahkan ContainerApp MataKuliah

--- app/src/main/java/com/example/pertemuan12/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan12/dependenciesinjection/ContainerApp.kt
@@ -3,14 +3,20 @@ package com.example.pertemuan12.dependenciesinjection
 import android.content.Context
 import com.example.pertemuan12.database.ProdiTI
 import com.example.pertemuan12.repository.LocalRepositoriDosen
+import com.example.pertemuan12.repository.LocalRepositoriMataKuliah
 import com.example.pertemuan12.repository.RepositoryDosen
+import com.example.pertemuan12.repository.RepositoryMataKuliah
 
 interface InterfaceContainerApp {
     val repositoryDosen: RepositoryDosen
+    val repositoryMataKuliah: RepositoryMataKuliah
 }
 
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
     override val repositoryDosen: RepositoryDosen by lazy {
         LocalRepositoriDosen(ProdiTI.getDatabase(context).dosenDao())
+    }
+    override val repositoryMataKuliah: RepositoryMataKuliah by lazy {
+        LocalRepositoriMataKuliah(ProdiTI.getDatabase(context).mataKuliahDao())
     }
 }
