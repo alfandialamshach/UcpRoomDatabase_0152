ed389ba4e71897dbac5554b3cb6cb1ba4fbc9bfb
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-18 15:24:51 +0700

    Membuat Alamatnavigasi

---
+++ app/src/main/java/com/example/pertemuan12/ui/Navigation/AlamaNavigasi.kt
@@ -0,0 +1,21 @@
+package com.example.pertemuan12.ui.Navigation
+
+interface Alamatnavigasi {
+    val route : String
+}
+
+object DestinasiHome : Alamatnavigasi {
+    override val route = "home" //untuk mendefinisikan route sebagai home
+}
+
+object DestinasiDetailMatakuliah : Alamatnavigasi {
+    override  val route = "detail" //untuk mendefinisikan route sebagai detail
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+
+object DestinasiUpdateMataKuliah : Alamatnavigasi {
+    override val route = "update" //untuk mendefinisikan route sebagai update
+    const val KODE = "kode"
+    val routeWithArg = "$route/{$KODE}"
+}
