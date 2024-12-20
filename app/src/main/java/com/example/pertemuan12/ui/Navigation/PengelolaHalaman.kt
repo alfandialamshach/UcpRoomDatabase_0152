9cb2bbafc45c0b00f37feb3c61536c469576714f
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-21 01:48:04 +0700

    Menambahkan destinasi detail matakuliah dan update

--- app/src/main/java/com/example/pertemuan12/ui/Navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan12/ui/Navigation/PengelolaHalaman.kt
@@ -3,9 +3,11 @@ package com.example.pertemuan12.ui.Navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.pertemuan12.ui.home.DestinasiHome
 import com.example.pertemuan12.ui.home.HomeView
 import com.example.pertemuan12.ui.view.Dosen.DestinasiTambahDosen
@@ -13,8 +15,10 @@ import com.example.pertemuan12.ui.view.Dosen.HomeDosenView
 import com.example.pertemuan12.ui.view.Dosen.TambahDosenView
 
 import com.example.pertemuan12.ui.view.MataKuliah.DestinasiTambah
+import com.example.pertemuan12.ui.view.MataKuliah.DetailMataKuliahView
 import com.example.pertemuan12.ui.view.MataKuliah.HomeMataKuliahView
 import com.example.pertemuan12.ui.view.MataKuliah.InsertMataKuliahView
+import com.example.pertemuan12.ui.view.MataKuliah.UpdateMataKuliahView
 
 @Composable
 fun PengelolaHalaman(
@@ -34,7 +38,7 @@ fun PengelolaHalaman(
         composable(route = DestinasiHomeMataKuliah.route) {
             HomeMataKuliahView(
                 onDetailClick = { kode ->
-                    navController.navigate("${DestinasiTambah.route}/$kode")
+                    navController.navigate("${DestinasiDetailMatakuliah.route}/$kode")
                 },
                 onAddMhs = {
                     navController.navigate(DestinasiTambah.route)
@@ -50,6 +54,31 @@ fun PengelolaHalaman(
                 onNavigate = { navController.popBackStack() },
                 modifier = modifier
             )
+        }
+
+        composable(
+            DestinasiDetailMatakuliah.routesWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetailMatakuliah.KODE){
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val kode = it.arguments?.getString(DestinasiDetailMatakuliah.KODE)
+            kode.let {kode ->
+                DetailMataKuliahView(
+                    onBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${DestinasiUpdateMataKuliah.route}/$it")
+                    },
+                    modifier = modifier,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
         }
 
         // Halaman Dosen
@@ -70,5 +99,24 @@ fun PengelolaHalaman(
                 modifier = modifier
             )
         }
+
+        composable(
+            DestinasiUpdateMataKuliah.routeWithArg,
+            arguments = listOf(
+                navArgument(DestinasiUpdateMataKuliah.KODE) {
+                    type = NavType.StringType })
+                )
+        {
+
+            UpdateMataKuliahView(
+                onBack = {
+                    navController.popBackStack() },
+                onNavigate = {
+                    navController.popBackStack() },
+                modifier = modifier
+            )
+        }
+
+
     }
 }
