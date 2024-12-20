a73645276d8d97c519359f52e8c59ff21b586613
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-20 22:24:33 +0700

    Menambah PengelolaHalaman untuk kembali

--- app/src/main/java/com/example/pertemuan12/ui/Navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan12/ui/Navigation/PengelolaHalaman.kt
@@ -6,6 +6,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan12.ui.home.DestinasiHome
+import com.example.pertemuan12.ui.home.HomeView
+import com.example.pertemuan12.ui.view.Dosen.DestinasiTambahDosen
+import com.example.pertemuan12.ui.view.Dosen.HomeDosenView
+import com.example.pertemuan12.ui.view.Dosen.TambahDosenView
+
+import com.example.pertemuan12.ui.view.MataKuliah.DestinasiTambah
+import com.example.pertemuan12.ui.view.MataKuliah.HomeMataKuliahView
 import com.example.pertemuan12.ui.view.MataKuliah.InsertMataKuliahView
 
 @Composable
@@ -14,16 +22,51 @@ fun PengelolaHalaman(
     modifier: Modifier = Modifier
 ) {
     NavHost(navController = navController, startDestination = DestinasiHome.route) {
-        composable(
-            route = DestinasiHome.route
-        ){
+        // Halaman Home
+        composable(route = DestinasiHome.route) {
+            HomeView(
+                onClickDosen = { navController.navigate(DestinasiHomeDosen.route) },
+                onClickMataKuliah = { navController.navigate(DestinasiHomeMataKuliah.route) }
+            )
+        }
+
+        // Halaman Mata Kuliah
+        composable(route = DestinasiHomeMataKuliah.route) {
+            HomeMataKuliahView(
+                onDetailClick = { kode ->
+                    navController.navigate("${DestinasiTambah.route}/$kode")
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiTambah.route)
+                },
+                onBack = { navController.popBackStack() },
+                modifier = modifier
+            )
+        }
+
+        composable(route = DestinasiTambah.route) {
             InsertMataKuliahView(
-                onBack = {
-                    navController.popBackStack()
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() },
+                modifier = modifier
+            )
+        }
+
+        // Halaman Dosen
+        composable(route = DestinasiHomeDosen.route) {
+            HomeDosenView(
+                onAddMhs = {
+                    navController.navigate(DestinasiTambahDosen.route)
                 },
-                onNavigate = {
-                    navController.popBackStack()
-                },
+                onBack = { navController.popBackStack()},
+                    modifier = modifier
+            )
+        }
+
+        composable(route = DestinasiTambahDosen.route) {
+            TambahDosenView(
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() },
                 modifier = modifier
             )
         }
