5bba8d29e0987628432488471e81b6baa3c2a11c
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-20 22:22:13 +0700

    Menambah object destinasi

--- app/src/main/java/com/example/pertemuan12/ui/Navigation/AlamaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan12/ui/Navigation/AlamaNavigasi.kt
@@ -4,8 +4,16 @@ interface Alamatnavigasi {
     val route : String
 }
 
-object DestinasiHome : Alamatnavigasi {
-    override val route = "home" //untuk mendefinisikan route sebagai home
+//object DestinasiHome : Alamatnavigasi {
+//    override val route = "home" //untuk mendefinisikan route sebagai home
+//}
+
+object DestinasiHomeDosen : Alamatnavigasi {
+    override val route = "home dosen" //untuk mendefinisikan route sebagai home
+}
+
+object DestinasiHomeMataKuliah : Alamatnavigasi {
+    override val route = "home matakuliah" //untuk mendefinisikan route sebagai home
 }
 
 object DestinasiDetailMatakuliah : Alamatnavigasi {
