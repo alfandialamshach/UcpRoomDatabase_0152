22f5b26679b8807a68f3df3a3ff9ce9e44629301
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-20 19:29:02 +0700

    Menambahkan HomeDosenViewModel

---
+++ app/src/main/java/com/example/pertemuan12/ui/view/Dosen/HomeDosenViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.pertemuan12.ui.view.Dosen
+
+
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.entity.Dosen
+import com.example.pertemuan12.repository.RepositoryDosen
+import com.example.pertemuan12.ui.view.MataKuliah.HomeUiState
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.WhileSubscribed
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeDosenViewModel (
+    private  val repositoryDosen: RepositoryDosen
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiStateDosen> = repositoryDosen.getAllDosen()
+        .filterNotNull()
+        .map {
+            HomeUiStateDosen(
+                listDosen = it.toList(),
+                isloadingDosen = false,
+
+                )
+        }
+        .onStart {
+            emit(HomeUiStateDosen(isloadingDosen = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiStateDosen(
+                    isloadingDosen = false,
+                    isError = true,
+                    errorMessageDosen = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiStateDosen(
+                isloadingDosen = true,
+            )
+        )
+}
+
+data class HomeUiStateDosen(
+    val listDosen: List<Dosen> = listOf(),
+    val isloadingDosen: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessageDosen: String = ""
+)
