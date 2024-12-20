05b11b87113fb0482c585b0b28df903369b6973a
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-20 19:30:18 +0700

    Menambahkan HomeMataKuliahViewModel

---
+++ app/src/main/java/com/example/pertemuan12/ui/view/MataKuliah/HomeMataKuliahViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.pertemuan12.ui.view.MataKuliah
+
+
+
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.entity.MataKuliah
+import com.example.pertemuan12.repository.RepositoryDosen
+import com.example.pertemuan12.repository.RepositoryMataKuliah
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.WhileSubscribed
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMataKuliahViewModel (
+    private  val repositoryMataKuliah: RepositoryMataKuliah,
+    private val repositoryDosen: RepositoryDosen
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMataKuliah.getAllMataKuliah()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMataKuliah = it.toList(),
+                isloading = false,
+
+                )
+        }
+        .onStart {
+            emit(HomeUiState(isloading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isloading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isloading = true,
+            )
+        )
+}
+
+data class HomeUiState(
+    val listMataKuliah: List<MataKuliah> = listOf(),
+    val isloading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
