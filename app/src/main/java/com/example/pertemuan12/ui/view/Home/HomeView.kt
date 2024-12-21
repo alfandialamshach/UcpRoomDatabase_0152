f28f3ee071cac111b1efc9c1a60bede9e2c01c61
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-22 00:27:02 +0700

    Merapikan tampilan HomeView

--- app/src/main/java/com/example/pertemuan12/ui/view/Home/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/Home/HomeView.kt
@@ -1,18 +1,21 @@
-package com.example.pertemuan12.ui.home
-
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pertemuan12.R
@@ -31,7 +34,13 @@ fun HomeView(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.LightGray)
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(Color(0xFF2196F3), Color.LightGray),
+                    startY = 0f,
+                    endY = 1000f
+                )
+            )
     ) {
         // Header Section
         Row(
@@ -40,39 +49,46 @@ fun HomeView(
                 .padding(40.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Box(
+                modifier = Modifier
+                    .size(90.dp)
+                    .background(Color.White, shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.umy),
+                    contentDescription = "Logo UMY",
+                    modifier = Modifier.size(70.dp)
+                )
+            }
 
-            Image(
-                painter = painterResource(id = R.drawable.umy),
-                contentDescription = "",
-                modifier = Modifier.size(80.dp)
-            )
-
-            Spacer(modifier = Modifier.width(30.dp))
+            Spacer(modifier = Modifier.width(20.dp))
 
             Column {
                 Text(
-                    text = "Hallo",
-                    color = Color.Black,
-                    fontSize = 48.sp,
-                    fontWeight = FontWeight.Bold
+                    text = "Halo,",
+                    color = Color.White,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.SemiBold
                 )
                 Text(
                     text = "Teknologi Informasi",
-                    color = Color.Red,
-                    fontSize = 26.sp,
+                    color = Color.Yellow,
+                    fontSize = 24.sp,
                     fontWeight = FontWeight.Bold
                 )
             }
         }
 
+        // Body Section
         Box(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .background(
                     color = Color.White,
                     shape = RoundedCornerShape(topEnd = 50.dp)
                 )
-                .padding(16.dp)
+                .padding(24.dp)
         ) {
             Column(
                 modifier = Modifier.fillMaxSize(),
@@ -80,38 +96,110 @@ fun HomeView(
             ) {
                 Spacer(modifier = Modifier.height(16.dp))
                 Text(
-                    text = "Menu",
-                    color = Color.Red,
-                    fontSize = 30.sp,
+                    text = "Menu Pilihan",
+                    color = Color(0xFF2196F3),
+                    fontSize = 28.sp,
                     fontWeight = FontWeight.Bold
                 )
+                Spacer(modifier = Modifier.height(16.dp))
 
-                Button(
-                    onClick = onClickDosen,
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFFFF9800),
-                        contentColor = Color.White
-                    ),
+                // Button Dosen
+                Card(
+                    shape = RoundedCornerShape(16.dp),
                     modifier = Modifier
                         .fillMaxWidth(0.9f)
-                        .padding(vertical = 8.dp)
+                        .padding(vertical = 8.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFFF9800))
                 ) {
-                    Text("Dosen")
+                    Button(
+                        onClick = onClickDosen,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color.White
+                        ),
+                        contentPadding = PaddingValues(16.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "Dosen",
+                            modifier = Modifier.size(28.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = "Dosen",
+                            fontSize = 20.sp
+                        )
+                    }
                 }
 
-                Button(
-                    onClick = onClickMataKuliah,
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFF2196F3),
-                        contentColor = Color.White
-                    ),
+                // Button Mata Kuliah
+                Card(
+                    shape = RoundedCornerShape(16.dp),
                     modifier = Modifier
                         .fillMaxWidth(0.9f)
-                        .padding(vertical = 8.dp)
+                        .padding(vertical = 8.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFF2196F3))
                 ) {
-                    Text("Mata Kuliah")
+                    Button(
+                        onClick = onClickMataKuliah,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color.White
+                        ),
+                        contentPadding = PaddingValues(16.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Info,
+                            contentDescription = "MataKuliah",
+                            modifier = Modifier.size(28.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = "MataKuliah",
+                            fontSize = 20.sp
+                        )
+                    }
                 }
+
+                // Animated Text
+                Spacer(modifier = Modifier.height(32.dp))
+                AnimatedFadingText(text = "Selamat datang di aplikasi kami!")
             }
         }
+    }
+}
+
+@Composable
+fun AnimatedFadingText(text: String,
+                       fontSize: TextUnit = 16.sp,
+                       fontWeight: FontWeight = FontWeight.Normal,
+                       color: Color = Color.Black,
+                       maxLines: Int = Int.MAX_VALUE) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val alpha by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = Color(0xFF2196F3).copy(alpha = alpha), // Mengatur transparansi menggunakan alpha
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .background(Color(0xFFE3F2FD)) // Menambahkan background dengan warna biru terang
+                .padding(8.dp) // Optional, to add padding around the text
+        )
+
     }
 }
