b551f4f67cf150aaba7d675632be6c8c8fb52df2
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-22 21:48:36 +0700

    menghapus Object yang tidak digunakan di Alamatnavigasi

--- app/src/main/java/com/example/pertemuan12/ui/Navigation/AlamaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan12/ui/Navigation/AlamaNavigasi.kt
@@ -4,26 +4,22 @@ interface Alamatnavigasi {
     val route : String
 }
 
-//object DestinasiHome : Alamatnavigasi {
-//    override val route = "home" //untuk mendefinisikan route sebagai home
-//}
-
 object DestinasiHomeDosen : Alamatnavigasi {
-    override val route = "home dosen" //untuk mendefinisikan route sebagai home
+    override val route = "home dosen" //untuk mendefinisikan route sebagai home Dosen
 }
 
 object DestinasiHomeMataKuliah : Alamatnavigasi {
-    override val route = "home matakuliah" //untuk mendefinisikan route sebagai home
+    override val route = "home matakuliah" //untuk mendefinisikan route sebagai home MataKuliah
 }
 
 object DestinasiDetailMatakuliah : Alamatnavigasi {
-    override  val route = "detail" //untuk mendefinisikan route sebagai detail
+    override  val route = "detail" //untuk mendefinisikan route sebagai detail MataKuliah
     const val KODE = "kode"
     val routesWithArg = "$route/{$KODE}"
 }
 
 object DestinasiUpdateMataKuliah : Alamatnavigasi {
-    override val route = "update" //untuk mendefinisikan route sebagai update
+    override val route = "update" //untuk mendefinisikan route sebagai update MataKuliah
     const val KODE = "kode"
     val routeWithArg = "$route/{$KODE}"
 }
